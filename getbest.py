1c05ad91791d937a24194e5ba74bd892d6426dc0
JesseVolstruis <schroder.jesse11@gmail.com>
2025-04-07 12:45:47 +0200

    Fixed code :)

--- getbest.py
+++ getbest.py
@@ -8,7 +8,7 @@ def getCols(f):
     i=1
     for head in headings:
         if head == "Student Number": num_col=i
-        elif head == "Mark" : mark_col = i
+        elif head == "Mark" : mark_col = i+1
     return (num_col, mark_col)
 
 def findTop(f,num_col, mark_col):
@@ -17,8 +17,10 @@ def findTop(f,num_col, mark_col):
     for line in f:
         data = line.strip().split(",")
         mark = int(data[mark_col])
+        idx = int(data[num_col])
         if mark > best:
             best=mark
+            best_idx=idx
     return best_idx, best
 
 f = open(sys.argv[1])
